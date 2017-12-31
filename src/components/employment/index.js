0d730fcd7c8f257bd19b73ea807034c13a28966b
Josh Jarmain <jjarmain@gmail.com>
2017-12-31 13:41:46 -0800

    fix warnings

--- src/components/employment/index.js
+++ src/components/employment/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import "./employment.css";
 export default ({employmentHistory}) => {
     return (
-        <div> {employmentHistory.map(employment => {
-            return (<div className="employmentWrapper">
+        <div> {employmentHistory.map((employment, idx) => {
+            return (<div key={idx} className="employmentWrapper">
                 <div className="employmentHeader"><span>{employment.company}</span> <span>{employment.location}</span> </div>
                 <div className="employmentSubHeader"><span>{employment.title}</span><span>{employment.years}</span> </div>
                 <div className="employmentDetails">
                     <div className="summaryTitle">Summary</div>
                     <div className="summaryDetails">
                         {
-                            employment.highlights.map(x => <div>{x}</div>)
+                            employment.highlights.map((x,index) => <div key={index}>{x}</div>)
                         }
                     </div>
                     <div className="summaryKeywords">{employment.keyWords.join(' ')}</div>
